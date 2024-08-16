93994ab8d2f8abceb11c861b3656300ab09ea067
Mulih <almulih@yahoo.com>
2024-08-16 23:32:09 +0300

    Seed initial progress data
    This commit adds a new file `trackProgress.js` in the `seedFolder` directory to seed initial progress data into the database. The `trackProgress.js` file defines an array of progress objects with properties such as user, goal, progressAmount, and date. The `seedProgress` function is called to populate the database with the initial progress data. This feature will provide pre-existing progress entries for users to track their progress.

---
+++ server/seedFolder/trackProgress.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+import ProgressSchema from '../models/Progress';
+
+const ProgressData = mongoose.model("Progress", ProgressSchema);
+
+const seedProgress = async () => {
+    try {
+        const existingData = await ProgressData.find();
+        if (existingData.length === 0) {
+            const initialProgress = [
+                {
+                    user: '626c8d7e4b5e6a7e4b5e6a7e',
+                    goal: '66bfb28da0e39b7a04adde1f',
+                    progressAmount: 20,
+                    date: new Date('2024-08-03')
+                },
+                {
+                    user: '626c8d7e4b5e6a7e4b5e6a7e',
+                    goal: '66bfb28da0e39b7a04adde1f',
+                    progressAmount: 22,
+                    date: new Date('2024-08-05')
+                },
+                {
+                    user: '626c8d7e4b5e6a7e4b5e6a7e',
+                    goal: '66bfb28da0e39b7a04adde1f',
+                    progressAmount: 25,
+                    date: new Date('2024-08-06')
+                },
+                {
+                    user: '626c8d7e4b5e6a7e4b5e6a7e',
+                    goal: '66bfb28da0e39b7a04adde1f',
+                    progressAmount: 29,
+                    date: new Date('2024-08-08')
+                },
+            ];
+            await ProgressModel.insertMany(initialProgress);
+            console.log('Progress entries seeded successfully.');
+        } else {
+            console.log('Progress entries already exist. Skipping ahead.');
+        }
+    } catch (error) {
+        console.log('Error inserting progress entries:', error.message);
+    }
+};
+
+export default seedProgress();
