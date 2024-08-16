f11a7bcb42b924e40590f5de15d4560ecb529f5c
Mulih <almulih@yahoo.com>
2024-08-16 22:31:12 +0300

    chore: Add GoalSchema to server/models/Goal.js

---
+++ server/models/Goal.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const GoalSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        type:{
+            type: String,
+            required: true
+        },
+        target: {
+            type: String,
+        },
+        frequency: {
+            type: String,
+        },
+        start_date: {
+            type: Date,
+        },
+        end_date: {
+            type: Date
+        },
+        UserId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    },
+)
+
+export default GoalSchema;
