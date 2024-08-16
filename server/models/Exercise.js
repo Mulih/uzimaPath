46ce9c705988491c2e237b1b5cdcbb1c8a26ce85
Mulih <almulih@yahoo.com>
2024-08-16 22:20:25 +0300

    feat: Add ExerciseSchema to server/models/Exercise.js
    This commit adds the ExerciseSchema to the server/models/Exercise.js file. The schema defines the structure of an Exercise document in the database, including fields such as username, type, sets, duration, date, and UserId. This schema will be used to store exercise data for users.

---
+++ server/models/Exercise.js
@@ -0,0 +1,45 @@
+/**
+ * ExerciseSchema defines the schema for an Exercise document in the database.
+ * It includes the following fields:
+ * - username: the username of the user who performed the exercise
+ * - type: the type of exercise (cardio, resistance, flexibility)
+ * - sets: an array of objects, each representing a set of the exercise.
+ *   Each set object includes the following fields:
+ *   - reps: the number of repetitions performed in the set
+ *   - weight: the weight used in the set (in pounds)
+ *   - duration: the duration of the set (in minutes)
+ * - duration: the total duration of the exercise (in minutes)
+ * - date: the date the exercise was performed
+ * - UserId: the ID of the User who performed the exercise
+ */
+import mongoose from 'mongoose';
+
+const ExerciseSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            enum: ['cardio', 'resistance', 'flexibility'], default: 'strength',
+        },
+        sets: [{
+            reps: { type: Number, required: true },
+            weight: { type: Number, required: true },
+            duration: { type: Number, required: true },
+        }],
+        duration: {
+            type: Number,
+            required: true
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        },
+        UserId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+
+    },
+)
+
+export default ExerciseSchema;
