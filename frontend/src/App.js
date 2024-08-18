60887c8efd46e5c101722aba015826daf456d4b2
Mulih <almulih@yahoo.com>
2024-08-18 18:52:58 +0300

    feat: Add lazy loading for images in Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.js';
+import Exercises from './pages/Exercises.js';
 function App() {
   return (
 
-    <div className="App">
-      <BrowserRouter>
-        <Home />
-      </BrowserRouter>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exercises" element={<Exercises />} />
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/pages/Exercises.js
+++ frontend/src/pages/Exercises.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchExercises, createExercise } from '../services/api.js';
 
   const Exercises = () =>  {
+    const [exercises, setExercises] = useState([]);
+
+    useEffect(() => {
+        fetchExercises()
+            .then(response => setExercises(response.data))
+            .catch(console.error);
+    }, []);
+
 	return (
 	  <div>
+        <h1>Exercises</h1>
+        <ul>
+            {exercises.map(exercise => (
+                <li key={exercise.id}>{exercise.name}</li>
+            ))}
+        </ul>
+        {/* Form to log new exercise */}
+        {/* Add more detailed exercise view here */}
 	  </div>
 	);
   }
