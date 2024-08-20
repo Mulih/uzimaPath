f5ace5d39af1355b97e0d9e284722a4ff6c3e961
Mulih <almulih@yahoo.com>
2024-08-20 18:46:28 +0300

    Update goalController.js to improve goal retrieval and update functionality

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -11,10 +11,23 @@ export const createGoal = async (req, res) => {
 };
 
 export const getGoals = async (req, res) => {
-    const { userId } = req.body.userId;
+
 
     try {
-        const goals = await Goal.findById(userId);
+        const goals = await Goal.find({}).sort({ createdAt: -1 });
+        res.status(200).json(goals);
+    } catch (error) {
+        res.status(500).json({ error: 'Couldnt display goals. Try again after a few minutes.' });
+    }
+};
+export const getGoal = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such goal exists' });
+    }
+
+    try {
+        const goals = await Goal.findById(id);
         res.status(200).json(goals);
     } catch (error) {
         res.status(500).json({ error: 'Couldnt display goals. Try again after a few minutes.' });
@@ -22,8 +35,14 @@ export const getGoals = async (req, res) => {
 };
 
 export const updateGoal = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such goal exists' });
+    }
     try {
-        const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        const updatedGoal = await Goal.findByIdAndUpdate({ _id: id }, {
+            ...req.body
+        });
         if (!updatedGoal) {
             return res.status(404).json({ error: 'No such goal exists' });
         }
@@ -33,8 +52,12 @@ export const updateGoal = async (req, res) => {
     }
 };
 export const deleteGoal = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such goal exists' });
+    }
     try {
-        const deletedGoal = await Goal.findByIdAndDelete(req.params.id);
+        const deletedGoal = await Goal.findByIdAndDelete({_id: id});
         if (!deletedGoal) {
             return res.status(404).json({ error: 'Goal not found.' });
         }
