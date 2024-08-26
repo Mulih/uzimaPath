9179b3c7538bdff94021561a9329ee22e66447c9
Mulih <almulih@yahoo.com>
2024-08-26 21:33:41 +0300

    Update useLogout hook to clear exercises context on logout
    The code changes in useLogout.js update the useLogout hook to clear the exercises context when a user logs out. This is achieved by dispatching a 'SET_EXERCISES' action with a payload of null to the exercises context. This chore improves the logout functionality and ensures that the exercises context is reset when a user logs out.

--- frontend/src/Hooks/useLogout.js
+++ frontend/src/Hooks/useLogout.js
@@ -1,11 +1,14 @@
 import { useAuthContext } from './useAuthContext.js';
+import { useExercisesContext } from './useExercisesContext.js';
 
 export const useLogout = () =>  {
     const { dispatch } = useAuthContext();
+    const { dispatch: dispatchExercises } = useExercisesContext();
 
     const logout = () => {
         localStorage.removeItem('user');
         dispatch({ type: 'LOGOUT' });
+        dispatchExercises({ type: 'SET_EXERCISES', payload: null });
     };
 
     return { logout };
